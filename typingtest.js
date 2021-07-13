6839190a5c517d2a343e000d98844ef5fe95f548
Josh <joshvelasqez6@gmail.com>
2021-07-12 18:00:33 -0600

    Add typing functionality in typing test

--- typingtest.js
+++ typingtest.js
@@ -58,7 +58,7 @@ document.body.addEventListener('keyup', function (e) {
 function size () {
     var size = keyboard.parentNode.clientWidth / 90;
     keyboard.style.fontSize = size + 'px';
-    console.log(size);
+    // console.log(size);
 }
 
 var keyboard = document.querySelector('.keyboard');
