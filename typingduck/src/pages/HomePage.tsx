ae47fca51e119735e79cabb95fc55247c3fb5341
Josh Velasquez <joshuavelasquez@Joshs-MacBook-Pro.local>
2024-05-02 10:13:23 -0600

    Fix home page layout

--- typingduck/src/pages/HomePage.tsx
+++ typingduck/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import KeyboardLayout from "../components/KeyboardLayout";
 import { useEffect, useState } from "react";
 
@@ -20,19 +20,21 @@ const HomePage = () => {
     return () => clearInterval(intervalId);
   }, []);
   return (
-    <Box sx={{ marginTop: 15 }}>
-      <Typography
-        variant="h2"
-        sx={{
-          height: 60,
-          color: "whitesmoke",
-          textAlign: "center",
-          marginBottom: 5,
-        }}
-      >
-        {typedText ? typedText : " "}
-      </Typography>
-      <KeyboardLayout animateWord={headerText} />
+    <Box sx={{ display: "flex", flexDirection: "column", marginTop: 15 }}>
+      <Container>
+        <Typography
+          variant="h2"
+          sx={{
+            height: 60,
+            color: "whitesmoke",
+            textAlign: "center",
+            marginBottom: 5,
+          }}
+        >
+          {typedText ? typedText : " "}
+        </Typography>
+        <KeyboardLayout animateWord={headerText} />
+      </Container>
     </Box>
   );
 };
