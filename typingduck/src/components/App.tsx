05ef741c6b95a60371119bd4eafb19df49d18d0d
Josh Velasquez <joshuavelasquez@Joshs-MacBook-Pro.local>
2024-05-02 09:31:24 -0600

    Switch routers to hash router for gh pages

--- typingduck/src/components/App.tsx
+++ typingduck/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import TypeTestPage from "../pages/TypeTestPage";
 import TypeSpeedPage from "../pages/TypeSpeedPage";
 import NavigationBar from "./NavigationBar";
@@ -6,14 +6,15 @@ import HomePage from "../pages/HomePage";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/TypingDuck">
+    // NOTE: we have to use HashRouter since BrowserRouter does not work with GitHub Pages
+    <HashRouter>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/typing-speed" element={<TypeSpeedPage />} />
         <Route path="/typing-test" element={<TypeTestPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
